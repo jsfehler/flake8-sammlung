50d6996abe8371eb10376d2b9589d79a63782fc9
Joshua Fehler <jsfehler@gmail.com>
2019-06-06 11:15:53 -0400

    Add tox for testing

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ from setuptools import setup
 
 setup(
     name="flake8-sammlung",
-    version="0.0.3",
     description="Collection of flake8 plugins",
     author="Joshua Fehler",
     author_email="jsfehler@gmail.com",
